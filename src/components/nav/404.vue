a5621f392c693f09863ec6ef9c63a6c9167ff1d0
Ronel Andaya <ronel.andaya@gmail.com>
2022-09-02 05:06:21 +0800

    013 Redirecting & Catch All Routes

---
+++ src/components/nav/404.vue
@@ -0,0 +1,5 @@
+<template >
+    <h2>
+        page not found may be go to <router-link to="/teams">Teams page</router-link>
+    </h2>
+</template>
--- src/main.js
+++ src/main.js
@@ -5,13 +5,16 @@ import App from './App.vue';
 import TeamsList from './components/teams/TeamsList.vue';
 import UsersList from './components/users/UsersList.vue';
 import TeamMembers from './components/teams/TeamMembers.vue';
+import four0four from './components/nav/404.vue';
 
 const router = createRouter({
     history: createWebHistory(), //to identify web history
     routes: [
+        {path:'/', redirect:'/teams'},
         {path:'/teams', component: TeamsList},
         {path:'/users', component: UsersList},
         {path:'/teams/:teamId', component: TeamMembers, props: true},
+        {path:'/:notFound(.*)', component: four0four}
     ]
 })
 
