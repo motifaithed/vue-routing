7c09d2d90353de4209cbe0a1fd1b31fb5e0331b8
Ronel Andaya <ronel.andaya@gmail.com>
2022-09-01 09:21:39 +0800

    003 Routing Setup

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
 
 import App from './App.vue';
+
+const router = createRouter({
+    history: createWebHistory(), //to identify web history
+    routes: []
+})
 
 const app = createApp(App)
 
