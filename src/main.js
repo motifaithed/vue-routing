c0236b58baa08f97b7b774dd0a6ef42d932fa3b2
Ronel Andaya <ronel.andaya@gmail.com>
2022-09-02 14:55:14 +0800

    017 Rendering Multiple Routes with Named Router Views

--- src/main.js
+++ src/main.js
@@ -6,15 +6,17 @@ import TeamsList from './components/teams/TeamsList.vue';
 import UsersList from './components/users/UsersList.vue';
 import TeamMembers from './components/teams/TeamMembers.vue';
 import four0four from './components/nav/404.vue';
+import TeamsFooter from './components/teams/TeamsFooter.vue';
+import UsersFooter from './components/users/UsersFooter.vue';
 
 const router = createRouter({
     history: createWebHistory(), //to identify web history
     routes: [
         {path:'/', redirect:'/teams'},
-        {name:'teams', path:'/teams', component: TeamsList, children:[
+        {name:'teams', path:'/teams', components:{default: TeamsList, footer: TeamsFooter} , children:[
             {name:'team-members', path:':teamId', component: TeamMembers, props: true}
         ]},
-        {path:'/users', component: UsersList},
+        {path:'/users', components:{default: UsersList, footer: UsersFooter } },
         {path:'/:notFound(.*)', component: four0four}
     ]
 })
