0c75564e0344e18430f2b5bb07589eebe7aa2ad5
Ronel Andaya <ronel.andaya@gmail.com>
2022-09-03 05:32:23 +0800

    024 Organizing Route Files

---
+++ src/router.js
@@ -0,0 +1,61 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+import TeamsList from './components/teams/TeamsList.vue';
+import UsersList from './components/users/UsersList.vue';
+import TeamMembers from './components/teams/TeamMembers.vue';
+import four0four from './components/nav/404.vue';
+import TeamsFooter from './components/teams/TeamsFooter.vue';
+import UsersFooter from './components/users/UsersFooter.vue';
+
+const router = createRouter({
+    history: createWebHistory(), //to identify web history
+    routes: [
+      { path: '/', redirect: '/teams' },
+      {
+        name: 'teams',
+        path: '/teams',
+        components: { default: TeamsList, footer: TeamsFooter },
+        children: [
+          {
+            name: 'team-members',
+            path: ':teamId',
+            component: TeamMembers,
+            props: true,
+          },
+        ],
+      },
+      { path: '/users', components: { default: UsersList, footer: UsersFooter } ,
+      beforeEnter(to,from,next){
+          console.log('users before enter');
+          console.log(to, from);
+          next();
+      }},
+      { path: '/:notFound(.*)', component: four0four },
+    ],
+    scrollBehavior(to, from, savedPosition) {
+      if (savedPosition) {
+        return savedPosition;
+      } else {
+        return { left: 0, top: 0 };
+      }
+    },
+  });
+
+  router.beforeEach((to, from, next) => {
+    console.log('Global Before Each');
+    console.log(to, from);
+    // if(to.name === 'team-members'){
+    //     next();
+    // }else{
+    //     next({name:'team-members', params:{teamId:'t2'}});
+    // }
+    next();
+  });
+  
+  router.afterEach((to,from)=>{
+      //you can use this to send analytics to where your users routed;
+      console.log('Global after each');
+      console.log(to,from);
+  })
+
+  export default router;
